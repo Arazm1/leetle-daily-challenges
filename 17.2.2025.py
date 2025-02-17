d51573ad6720061fb9df8112cdc78a8002ee8235
Arazm1 <arazm@metropolia.fi>
2025-02-17 23:15:05 +0200

    Update 17.2.2025.py

--- 17.2.2025.py
+++ 17.2.2025.py
@@ -1,3 +1,11 @@
+#Write a function solve that adds two binary strings and returns their sum as a binary string.
+
+#Example:
+#Input: "11", "1"
+#Output: "100" 
+
+#Make sure you return your solution, don't print!
+
 def solve(a, b):
   number1 = int(a, 2)
   number2 = int(b, 2)
